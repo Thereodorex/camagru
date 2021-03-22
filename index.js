fd67511ca98fd64a810d7378a424379656cca499
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-03-22 20:58:23 +0300

    Init

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-// const express = require('express'),
-import express from 'express';
-
-import { sendHtml } from './utils/sendHtml';
-
-http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
+const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + "/public/login/login.html");
+})
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  sendHtml(res, './index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+})
 
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000)
